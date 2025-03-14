5cd47ca0aa087d5882e94daf2be5537f68c4bf9b
Thais dos Santos Figueiredo <figueiredothais652@gmail.com>
2025-03-14 16:17:20 -0300

    Add files via upload

---
+++ Real.java
@@ -0,0 +1,23 @@
+package media;
+
+import java.util.Scanner;
+
+public class Real {
+
+	public static void main(String[] args) {//inicio
+		double n1;
+		double n2;
+		
+		System.out.printf("Algoritmo para calcular entre dois números:");
+		System.out.printf("digite o primeiro número:");
+		Scanner ler = new Scanner(System.in);
+		n1 = ler.nextDouble();
+		System.out.printf("digite o segundo número:");
+		n2 = ler.nextDouble();
+		double media = (n1+n2)/2;
+		System.out.printf(" o valor da média é:%f",media);
+		
+		
+	}
+}
+
